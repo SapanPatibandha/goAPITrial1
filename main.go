e5a5047a751d0f47c7fd7141c5efbfe12b79e480
Sapan Patibandha <sapan.patibandha@contis.com>
2022-12-22 10:21:11 +0530

    function to handler endpoint created

--- main.go
+++ main.go
@@ -1,11 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 type Movie struct {
-	Id       string    `json:"movieid"`
-	Name     string    `json:"moviename"`
-	Director *Director `json:"director"`
+	Id       string   `json:"movieid"`
+	Name     string   `json:"moviename"`
+	Director Director `json:"director"`
 }
 
 type Director struct {
@@ -13,15 +20,73 @@ type Director struct {
 	LastName  string `json:"lastname"`
 }
 
+var movies []Movie
+
 func main() {
 
 	fmt.Println("API starts")
 
 	//Create router
+	r := mux.NewRouter()
+	fmt.Println("Router started")
 
 	//seed testing data
+	movies = append(movies,
+		Movie{
+			Id:   "1",
+			Name: "Aavtar 2",
+			Director: Director{
+				FirstName: "Sapan",
+				LastName:  "Patibandha",
+			},
+		},
+		Movie{
+			Id:   "2",
+			Name: "Eagle Eye",
+			Director: Director{
+				FirstName: "Mark",
+				LastName:  "Dunston",
+			},
+		},
+	)
+
+	fmt.Printf("%v", movies)
 
 	//handle routes
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("<h1>Welcome to Golang API</h1>"))
+	})
+	r.HandleFunc("/movies", getAllMovies).Methods("GET")
+	r.HandleFunc("/movie/{id}", getMovie).Methods("GET")
+	r.HandleFunc("/movie", createMovie).Methods("POST")
+	r.HandleFunc("/movie/{id}", updateMovie).Methods("PUT")
+	r.HandleFunc("/movie/{id}", deleteMovie).Methods("DELETE")
 
 	//liston to the port.
+	log.Fatal(http.ListenAndServe(":8001", r))
+
+}
+
+func getAllMovies(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Get all movies.")
+
+	w.Header().Set("Content-Type", "applicaiton/json")
+
+	json.NewEncoder(w).Encode(movies)
+}
+
+func getMovie(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func createMovie(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func updateMovie(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func deleteMovie(w http.ResponseWriter, r *http.Request) {
+
 }
