d35e229bd06acbd130c0a8fb238dd0014da8f099
Sapan Patibandha <sapan.patibandha@contis.com>
2022-12-23 15:07:41 +0530

    update method is created

--- main.go
+++ main.go
@@ -123,7 +123,27 @@ func createMovie(w http.ResponseWriter, r *http.Request) {
 }
 
 func updateMovie(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("update specific movie")
+	w.Header().Set("Content-Type", "applicaiton/json")
 
+	params := mux.Vars(r)
+
+	for index, movie := range movies {
+		if movie.Id == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+			var movie Movie
+			json.NewDecoder(r.Body).Decode(&movie)
+
+			movie.Id = params["id"]
+			movies = append(movies, movie)
+
+			json.NewEncoder(w).Encode(movie)
+			return
+		}
+	}
+
+	json.NewEncoder(w).Encode("no record found")
+	return
 }
 
 func deleteMovie(w http.ResponseWriter, r *http.Request) {
